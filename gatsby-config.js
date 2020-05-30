d6d1b90842cc18b2fdc3ce07cecc74e44ed3bc2d
oruiz622 <oruiz622@gmail.com>
2020-05-29 22:20:53 -0400

    second env fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,9 +32,9 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: CONTENTFUL_SPACE_ID,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
         // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: CONTENTFUL_ACCESS_TOKEN,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     `gatsby-plugin-sass`,
